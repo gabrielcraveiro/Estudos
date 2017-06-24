2ae13292bc66d37ed1e9e014344197f8cd106a12
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-06-24 16:03:15 -0300

    Projetos do webdevbtc

--- Jquery/script.js
+++ Jquery/script.js
@@ -1,15 +1,25 @@
-function divisors(integer) {
-  let i = 2;
-  let array = [];
-  while (i < integer) {
-    if (integer % i == 0 && i) {
-      array.push(i);
-    }
-    i++
+function helloWorld() {
+  console.log('Hello World'); // Hello World
+  console.log(this); // window...
+}
+
+// Dentro de um objeto sempre referencia o proprio objeto
+
+var santos = {
+  pontes: 12,
+  aeroportos: 0,
+  times: 1,
+  numeroPontes: function() {
+    console.log('Existem ' + this.pontes + ' pontes em Santos!'); // 12
   }
-  if (array.length === 0) {
-    return `${integer} is prime`
-  } else {
-    return array;
-  }
-};
+}
+
+// Como um construtor
+
+var Cidade = function(nome, estado) {
+  this.nome = nome || 'Santos'; // default para Santos
+  this.estado = estado || 'São Paulo';
+}
+
+santos = new Cidade(); // Cidade {nome: 'Santos', estado: 'São Paulo'}
+curitiba = new Cidade('Curitiba','Parana'); // Cidade {nome: 'Curitiba', estado: 'Parana'}
