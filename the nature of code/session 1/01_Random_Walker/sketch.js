8a099a281fccaeeeb9089bef714acfd8fb0f08c5
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2017-07-23 21:32:09 -0300

    NOC - 1.4

--- the nature of code/session 1/01_Random_Walker/sketch.js
+++ the nature of code/session 1/01_Random_Walker/sketch.js
@@ -2,32 +2,49 @@ var w;
 
 function setup() {
   createCanvas(800, 1000);
-  // Make a Walker object
+  // Faz o walker andar
   w = new Walker();
 }
 
 function draw() {
   background(51);
-  // Update and display object
+  // Update e display do objeto
   w.update();
   w.display();
 }
 
 function Walker() {
 
-  // Start Walker in center
-  this.pos = createVector(width / 2, 0);
+  // Começa com objeto no centro
+  this.pos = createVector(width / 2, height / 2);
   this.vel = createVector(0, 0);
-  this.acc = createVector(0, 0.1);
 
   this.update = function () {
     // Move Walker randomly
+
+    // Aceleração linear
+    // this.acc = createVector(0, 0.1);
+
+    // Aceleração Aleatória
+    // this.acc = createVector(random(-0.1, 0.1), random(-0.1, 0.1));
+    // this.acc = p5.Vector.fromAngle(random(TWO_PI));
+    // this.acc.mult(-2);
+
+    // Aceleração direcionada para o mouse
+    var mouse = createVector(mouseX, mouseY);
+    this.acc = p5.Vector.sub(mouse, this.pos);
+    // this.acc.normalize();
+    // this.acc.mult(0.1);
+    this.acc.setMag(0.05);
+
     this.vel.add(this.acc);
     this.pos.add(this.vel);
+
+    // Normalize pega um vetor de qq tamanho e muda pra magnitude 1
   }
 
   this.display = function () {
-    // Draw Walker as circle
+    // Faz o walker ser um circulo
     fill(255);
     ellipse(this.pos.x, this.pos.y, 48, 48);
   }
